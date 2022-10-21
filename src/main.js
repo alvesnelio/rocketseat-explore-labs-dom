5c4c832d475bdc39d300d9492b7aaf8c3aab3e4b
Nélio Alves <nelio13alves@gmail.com>
2022-10-20 23:30:59 -0400

    feat(Development): :sparkles: Implementação de codigos da primeira aula.
    https://www.youtube.com/watch?v=om_QLx6stVI

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-import "./css/index.css"
+import "./css/index.css";
+
+const cgBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const cgBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
+
+function setCardType(type) {
+  const colors = {
+    visa: [
+      "purple", "brown"
+    ],
+    mastercard: [
+      "red", "blue"
+    ],
+    default: ["#000000", "#ffffff"]
+  };
+
+  cgBgColor01.setAttribute("fill", colors[type][0]);
+  cgBgColor02.setAttribute("fill", colors[type][1]);
+
+  // if()
+  const path = `cc-${type}.svg`;
+  ccLogo.setAttribute("src", path);
+}
+
+globalThis.setCardType = setCardType;
